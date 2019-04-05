960a0ed95961ecc9d9244de9a3674362ea4db4d8
Charles Taylor <charlestaylor95@gmail.com>
2019-04-05 18:16:47 -0400

    Refactor; fix styling on text input

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,35 +1,11 @@
 import React, { useState } from 'react';
 import File from './File';
 
-const getCounter = () => {
-  let count = 0;
-  return () => count++;
-};
-
-const nextItemId = getCounter();
-
-const getItemElement = item => {
-  return item.type === 'FOLDER' ? (
-    <Folder key={item.id} folder={item} />
-  ) : (
-    <File key={item.id} file={item} />
-  );
-};
-
 const Folder = ({ folder }) => {
   const [open, setOpen] = useState(false);
   const [children, setChildren] = useState([]);
-  const [mode, setMode] = useState('VIEW');
 
-  const buttonStyle = {
-    marginLeft: '10px',
-    background: 'grey',
-    border: 'none',
-    outline: 'none'
-  };
-
-  const onSubmit = newItem => {
-    setMode('VIEW');
+  const addItemToFolder = newItem => {
     setOpen(true);
     setChildren(children => [...children, getItemElement(newItem)]);
   };
@@ -49,18 +25,8 @@ const Folder = ({ folder }) => {
         {open ? '-' : '+'}
       </button>
       {folder.name}
-      {mode === 'VIEW' ? (
-        <>
-          <button style={buttonStyle} onClick={() => setMode('FILE')}>
-            + New File
-          </button>
-          <button style={buttonStyle} onClick={() => setMode('FOLDER')}>
-            + New Folder
-          </button>
-        </>
-      ) : (
-        <ItemInput type={mode} onSubmit={onSubmit} />
-      )}
+      <InputItem addItemToFolder={addItemToFolder} />
+
       <ul
         style={{
           listStyleType: 'none',
@@ -76,29 +42,72 @@ const Folder = ({ folder }) => {
 
 export default Folder;
 
-const ItemInput = ({ onSubmit, type }) => {
+const getCounter = () => {
+  let count = 0;
+  return () => count++;
+};
+
+const nextItemId = getCounter();
+
+const getItemElement = item =>
+  item.type === 'FOLDER' ? (
+    <Folder key={item.id} folder={item} />
+  ) : (
+    <File key={item.id} file={item} />
+  );
+
+const InputItem = ({ addItemToFolder }) => {
+  const [mode, setMode] = useState('VIEW');
+
+  const buttonStyle = {
+    marginLeft: '10px',
+    background: 'grey',
+    border: 'none',
+    outline: 'none'
+  };
+
+  const addItem = newItem => {
+    setMode('VIEW');
+    addItemToFolder(newItem);
+  };
+
+  return mode === 'VIEW' ? (
+    <>
+      <button style={buttonStyle} onClick={() => setMode('FILE')}>
+        + New File
+      </button>
+      <button style={buttonStyle} onClick={() => setMode('FOLDER')}>
+        + New Folder
+      </button>
+    </>
+  ) : (
+    <ItemTextInput type={mode} addItem={addItem} />
+  );
+};
+
+const ItemTextInput = ({ addItem, type }) => {
   const [input, setInput] = useState('');
 
   const onKeyPress = event => {
-    if (event.key === 'Enter') handleSubmit();
+    if (event.key === 'Enter') onSubmit();
   };
 
-  const handleSubmit = () => {
+  const onSubmit = () => {
     const name = input;
     const id = nextItemId();
-    onSubmit({ id, name, type });
+    addItem({ id, name, type });
   };
 
   return (
     <input
       type='text'
-      style={{ margin: '10px' }}
+      style={{ marginLeft: '10px', border: 'none' }}
       value={input}
       placeholder={`Enter ${type.toLowerCase()} name`}
       autoFocus
       onChange={event => setInput(event.target.value)}
       onKeyPress={onKeyPress}
-      onBlur={handleSubmit}
+      onBlur={onSubmit}
     />
   );
 };
