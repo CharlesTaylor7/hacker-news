be0d9a7e1403c5da0d7924e3d015fde26d2eec0f
Charles Taylor <charlestaylor95@gmail.com>
2019-04-05 11:04:35 -0700

    Able to add new files

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import './App.css';
 import Folder from './Folder';
+import File from './File';
 
 export default () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
         <Folder folder={{ name: 'Project', id: 'root' }} />
       </header>
     </div>
--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
+import File from './File';
 
 export default ({ folder }) => {
   const [open, setOpen] = useState(false);
+  const [children, setChildren] = useState([]);
+  const [addingItem, setAddingItem] = useState(false);
+
+  const newItemButton = (
+    <button
+      style={{
+        margin: '10px',
+        background: 'grey',
+        border: 'none',
+        outline: 'none'
+      }}
+      onClick={() => setAddingItem(true)}
+    >
+      + New Item
+    </button>
+  );
+
+  const itemInput = (
+    <ItemInput
+      onSubmit={input => {
+        setAddingItem(false);
+        setOpen(true);
+        setChildren(children => [...children, <File file={{ name: input }} />]);
+      }}
+    />
+  );
+
   return (
-    <div style={{ flex: 'inline' }}>
+    <li key={folder.key} style={{ display: 'inline' }}>
       <button
         style={{
           margin: '10px',
@@ -17,7 +45,22 @@ export default ({ folder }) => {
         {open ? '-' : '+'}
       </button>
       {folder.name}
-      <ul style={{ listStyleType: 'none' }} />
-    </div>
+      {addingItem ? itemInput : newItemButton}
+      <ul style={{ listStyleType: 'none' }}>{open ? children : null}</ul>
+    </li>
+  );
+};
+
+const ItemInput = ({ onSubmit }) => {
+  const [input, setInput] = useState('');
+  const [isFolder, setIsFolder] = useState(false);
+  return (
+    <input
+      type='text'
+      style={{ margin: '10px' }}
+      value={input}
+      onChange={event => setInput(event.target.value)}
+      onKeyPress={event => (event.key === 'Enter' ? onSubmit(input) : null)}
+    />
   );
 };
