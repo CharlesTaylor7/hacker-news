61bbd04e60b3ec65e1f17696d803e7d0726a1ee1
Charles Taylor <charlestaylor95@gmail.com>
2024-04-21 15:00:23 -0400

    Rename

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hn-client/',
+  base: '/hacker-news/',
   plugins: [react()],
   esbuild: {
     keepNames: true,
