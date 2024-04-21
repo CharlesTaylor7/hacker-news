cab39d71b31397fc52bdf543a098cdfc54b52bb9
Charles Taylor <charlestaylor95@gmail.com>
2024-04-21 19:45:10 -0400

    Simplify

--- src/HackerNewsAPI.js
+++ src/HackerNewsAPI.js
@@ -1,18 +1,11 @@
 const rootURL = 'https://hacker-news.firebaseio.com/v0';
 const suffix = '.json';
 
-export const getLatestId = () =>
-  fetch(`${rootURL}/maxitem${suffix}`).then(response => response.json());
+export async function getItem(itemId) {
+  return fetch(`${rootURL}/item/${itemId}${suffix}`).then(response => response.json());
+}
 
-export const getItem = itemId =>
-  fetch(`${rootURL}/item/${itemId}${suffix}`).then(response => response.json());
-
-export const getUpdates = () =>
-  fetch(`${rootURL}/updates${suffix}`)
-    .then(response => response.json())
-    .then(json => json.items);
-
-export const getRoot = async id => {
+export async function getRoot(id) {
   while (true) {
     const item = await getItem(id);
     if (item === null) {
