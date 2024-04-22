76e2b57fe9161f9c884fae8bec1a1f42639e16f0
Charles Taylor <charlestaylor95@gmail.com>
2024-04-22 10:16:19 -0400

    Use esm for vite

---
+++ vite.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwind from 'tailwindcss';
+
+export default defineConfig({
+  base: '/hacker-news/',
+  plugins: [react()],
+  esbuild: {
+    keepNames: true,
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwind,
+      ]
+    },
+  },
+  optimizeDeps: {
+    force: true,
+  },
+})
